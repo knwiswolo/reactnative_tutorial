c887980ca2064360dce60dd3fa76419765695e14
Wiz <Wiz@Maverick-MBP.local>
2018-01-18 16:50:18 +0100

    Styling - Text & View

--- albums/index.js
+++ albums/index.js
@@ -2,3 +2,5 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 
 AppRegistry.registerComponent('albums', () => App);
+// Render it to the device
+ReactNative.AppRegistry.registerComponent('albums', () => App);
